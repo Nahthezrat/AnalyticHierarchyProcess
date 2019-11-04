7566a605debdf458d8e1771982cf2ae466652689
NahtHezrat <nahthezrat@gmail.com>
2019-11-04 12:59:41 -0600

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyticHierarchyProcess
+{
+    class Matrix
+    {
+        public double[,] Elements;
+
+        Matrix(double[,] inputMatrix)
+        {
+            Elements = (double[,])inputMatrix.Clone();
+        }
+    }
+}
